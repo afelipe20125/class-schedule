326ddaccccdac5f628570933253cea014e32c90f
20125 <20125@MININT-HADV8T3.columbushs.com>
2019-08-30 14:56:34 -0400

    blah

--- src/pkgclass/schedule/ClassSchedule.java
+++ src/pkgclass/schedule/ClassSchedule.java
@@ -15,7 +15,23 @@ public class ClassSchedule {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String subject7 = "Hon computerapp";
+        String teacher7 = "Larrubia";
+        String teacher6 = "Moscoso";
+        String subject6 = "English11";
+        String teacher5 = "mccullagh";
+        String subject5 = "hon US gov";
+        String teacher4 = "ciocca";
+        String subject4 = "Hon Micro";
+        String teacher3 = "Davis";
+        String subject3 = "Hon socialjustice";
+        String teacher2 = "ferguson";
+        String subject2 = "college alg&trig";
+        String teacher1 = "zagales";
+        String subject1 = "Hon ww2";
+       
+        
+        
     }
     
 }
